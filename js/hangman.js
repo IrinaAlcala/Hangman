2c03b13c0497988d87eb0304b738603722125634
IrinaAlcala <58370677+IrinaAlcala@users.noreply.github.com>
2020-09-16 15:46:24 -0700

    added function button

---
+++ js/hangman.js
@@ -0,0 +1,53 @@
+var programming_languages = [
+    "python",
+	"javascript",
+	"mongodb",
+	"json",
+	"java",
+	"html",
+	"css",
+	"c",
+	"csharp",
+	"golang",
+	"kotlin",
+	"php",
+	"sql",
+	"ruby"   
+]
+
+let answer = '';
+let maxWrong = 6;
+let mistakes = 0;
+let guessed = [];
+let wordStatus = null;
+
+function randomWord() {
+    answer = programming_languages[Math.floor(Math.random() * programming_languages.length)];
+
+}
+
+function generateButtons() {
+    let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+      `
+        <button
+          class="btn btn-lg btn-primary m-2"
+          id='` + letter + `'
+          onClick="handleGuess('` + letter + `')"
+        >
+          ` + letter + `
+        </button>
+      `).join('');
+
+      document.getElementById('keyboard').innerHTML = buttonsHTML;
+}
+
+function reset() {
+    mistakes = 0;
+    guessed = [];
+    document.getElementById('hangmanPic').src = './images/0.jpg';
+
+    randomWord();
+    generateButtons();
+    guessedWord();
+    updateMistakes();
+}
